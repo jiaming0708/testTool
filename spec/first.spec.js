a4126e76582686bbac4f2642ca838632266482fc
Jimmy <jimmy@wishingsoft.com>
2017-10-18 16:14:45 +0800

    more machers

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -2,4 +2,16 @@ describe('frist test', () => {
     it('hello jasmine', () => {
         expect(true).toBe(true);
     });
+
+    it('toEqual', () => {
+        let a = 12;
+        expect(a).toEqual(12);
+    });
+
+    it('less & greater', () => {
+        let pi = 3.14159;
+        let e = 2.78;
+        expect(pi).not.toBeLessThan(e);
+        expect(pi).toBeGreaterThan(e);
+    })
 });
