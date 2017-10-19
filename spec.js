b631342658cd2daf0559d79343b6f8f5b7e3f874
Jimmy <jimmy@wishingsoft.com>
2017-10-19 09:53:34 +0800

    click button and get result value

--- spec.js
+++ spec.js
@@ -17,4 +17,16 @@ describe('Protractor Demo App', function () {
         // let sec = els.last(); same as get(1)
         sec.sendKeys('4');
     });
+
+    fit('calculate', () => {
+        let els = element.all(by.css('.input-small'));
+        let first = els.get(0);
+        // let first = els.first(); same as get(0)
+        first.sendKeys('2');
+        let sec = els.get(1);
+        // let sec = els.last();
+        sec.sendKeys('4');
+        element(by.id('gobutton')).click();
+        expect($('h2').getText()).toEqual('6');
+    })
 });
